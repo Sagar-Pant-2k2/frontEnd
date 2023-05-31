62c26ec0b456a37e79428409ee04f954f36cce35
sagar_pant <sagarpant2k2@gmail.com>
2023-05-31 12:51:03 +0530

    firstCommit

---
+++ script.js
@@ -0,0 +1,10 @@
+Array.from(document.querySelectorAll('.addToCart')).map((x)=>{
+    x.addEventListener('click',()=>{
+        alert("not login");
+})})
+
+Array.from(document.querySelectorAll('.addToWishlist')).
+map((x)=>
+{   x.addEventListener('click',()=>{
+    alert("not login");})})
+
