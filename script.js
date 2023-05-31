c28c3317cf74f9a1ba2e2c96786a1e49b72288b3
sagar_pant <sagarpant2k2@gmail.com>
2023-05-31 16:32:45 +0530

    connected some points

--- script.js
+++ script.js
@@ -1,108 +1,138 @@
-const modalOverlay = document.querySelector('.modalOverlay');
-Array.from(document.querySelectorAll('.addToCart')).map((x)=>{
-    x.addEventListener('click',()=>{
-        alert("not login");
-})})
-
-Array.from(document.querySelectorAll('.addToWishlist')).
-map((x)=>
-{   x.addEventListener('click',()=>{
-    alert("not login");})})
-
-
-
-
-const loginPage = `<div class="modalOverlay">
+const registerLoginPage = `<div class="modalOverlay">
 
 <div class="modal">
-<div class="upperModal">
+
   <div>
     <h1>GBmart</h1>
   </div>
-  <div>
-    <h2>Member Login</h2>
-  </div>
-  <div>
+  <img src="./logo.jpg" style="max-width: 100%; object-fit: cover; ">
+ 
+  <span>
 
-    <p>Access your <b>GBmart</b> account</p>
-  </div>
- </div>
- <div class="lowerModal">
-  <div>
-    <input type="text" placeholder="UserEmail" class="inputFields">
-  </div>
-  <div>
-    <input type="password" placeholder="password" class="inputFields">
-  </div>
-  <div>
-    <input type="checkbox">Remember me on this computer
-  </div>
-   <button id="login">Login</button>
+    <button class="login">Login</button>
+    <button class="register">Register</button>
+  </span>
  </div>
 </div>
 </div>`;
-
-const registerPage = `<div class="modalOverlay">
+    
+    const loginPage = `<div class="modalOverlay">
+    
+    <div class="modal">
+    <div class="upperModal">
+    <div>
+    <h1>GBmart</h1>
+    </div>
+    <div>
+    <h2>Member Login</h2>
+    </div>
+    <div>
+    
+    <p>Access your <b>GBmart</b> account</p>
+    </div>
+    </div>
+    <div class="lowerModal">
+    <div>
+    <input type="text" placeholder="UserEmail" class="inputFields">
+    </div>
+    <div>
+    <input type="password" placeholder="password" class="inputFields">
+    </div>
+    <div>
+    <input type="checkbox">Remember me on this computer
+    </div>
+    <button id="login">Login</button>
+    </div>
+    </div>
+    </div>`;
+    
+    const registerPage = `<div class="modalOverlay">
 
 <div class="modal registrationForm">
 <div class="upperModal">
-  <div>
-    <h1>GBmart</h1>
-  </div>
-  <div>
-    <h2>Member Login</h2>
-  </div>
-  <div>
+<div>
+<h1>GBmart</h1>
+</div>
+<div>
+<h2>Member Login</h2>
+</div>
+<div>
 
-    <p>Access your <b>GBmart</b> account</p>
-  </div>
- </div>
- <div class="lowerModal">
-  <div>
-    <input type="text" placeholder="UserName" class="inputFields">
-  </div>
-  <div>
-    <input type="email" placeholder="xyz@abc.jkl" class="inputFields">
-  </div>
-  <div>
-    <input type="password" placeholder="password" class="inputFields">
-  </div>
-  <div>
-    <input type="checkbox">Remember me on this computer
-  </div>
-   <button id="register">Register</button>
- </div>
+<p>Access your <b>GBmart</b> account</p>
+</div>
+</div>
+<div class="lowerModal">
+<div>
+<input type="text" placeholder="UserName" class="inputFields">
+</div>
+<div>
+<input type="email" placeholder="xyz@abc.jkl" class="inputFields">
+</div>
+<div>
+<input type="password" placeholder="password" class="inputFields">
+</div>
+
+<button id="register">Register</button>
+</div>
 </div>
 </div>`
+Array.from(document.querySelectorAll('.addToCart')).map((x)=>{
+    x.addEventListener('click',()=>{
+        const modalOverlay = document.querySelector('.modalOverlay');
+        if (modalOverlay && document.body.contains(modalOverlay)) {
+            document.body.removeChild(modalOverlay);
+        }
+        document.body.insertAdjacentHTML('afterbegin',registerLoginPage);
+    })})
+    
+Array.from(document.querySelectorAll('.addToWishlist')).
+    map((x)=>{
+        x.addEventListener('click',()=>{
+            if (modalOverlay && document.body.contains(modalOverlay)) {
+                document.body.removeChild(modalOverlay);
+            }
+        document.body.insertAdjacentHTML('afterbegin',registerLoginPage);
+    })})
+
+
 
 // opening of registration window
 const openRegister = ()=>{
+    const modalOverlay = document.querySelector('.modalOverlay');
     if (modalOverlay && document.body.contains(modalOverlay)) {
         document.body.removeChild(modalOverlay);
     }
-    document.body.innerHTML=registerPage;
+    document.body.insertAdjacentHTML('afterbegin',registerPage);
+    modalOverlay = document.querySelector('.modalOverlay');
+    modalOverlay.addEventListener('click',()=>{
+        if (modalOverlay && document.body.contains(modalOverlay)) {
+            document.body.removeChild(modalOverlay);
+        }
+        console.log("done");
+    })  
 }
 
 
 // opening of login window
 const openLogin = ()=>{
+    const modalOverlay = document.querySelector('.modalOverlay');
     if (modalOverlay && document.body.contains(modalOverlay)) {
         document.body.removeChild(modalOverlay);
       }
-    document.body.innerHTML=loginPage;
+    document.body.insertAdjacentHTML('afterbegin',registerLoginPage);
+    modalOverlay = document.querySelector('.modalOverlay');
+    modalOverlay.addEventListener('click',()=>{
+        if (modalOverlay && document.body.contains(modalOverlay)) {
+            document.body.removeChild(modalOverlay);
+        }
+        console.log("done");
+    })
 }
 
 
 
 // function for closing modal windows
-Array.from(document.querySelectorAll('.modalOverlay')).map((x)=>{
-    x.addEventListener('click',()=>{
-    if (modalOverlay && document.body.contains(modalOverlay)) {
-        document.body.removeChild(modalOverlay);
-    }
-    console.log("done");
-})
-})
+
 
 
 
